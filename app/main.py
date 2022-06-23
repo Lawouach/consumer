8f5c1669ad8d150f1f9a6308264e09668b668dc9
Sylvain Hellegouarch <sh@defuze.org>
2022-06-23 16:07:51 +0200

    set root path
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- app/main.py
+++ app/main.py
@@ -23,14 +23,14 @@ def set_latency(value: float) -> None:
     latency = value
 
 
-@app.get("/")
+@app.get("/consumer")
 def index() -> Dict[str, str]:
     if latency > 0:
         time.sleep(latency)
     return {"Hello": "The World"}
 
 
-@app.get("/data")
+@app.get("/consumer/data")
 def data() -> Dict[str, Any]:
     if latency > 0:
         time.sleep(latency)
@@ -44,7 +44,7 @@ def health() -> str:
     return ""
 
 
-@app.get("/inject/latency")
+@app.get("/consumer/inject/latency")
 def inject_latency(value: float = 0) -> str:
     set_latency(value)
     return ""
